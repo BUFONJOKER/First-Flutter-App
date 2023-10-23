6ece0d1ebaa6677e4708a945565cb28d887236cf
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-10-23 12:01:58 +0500

    Day 1 font famiy added and background color changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar:AppBar(
-        title:const Text('My First Flutter App'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child:Text("Hello World"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: const Text('Button'),
-      )
-    ),
+        appBar: AppBar(
+          title: const Text('My First Flutter App'),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[600],
+        ),
+        body: const Center(
+          child: Text(
+            "Hello World",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontStyle: FontStyle.italic,
+              letterSpacing: 2,
+              color: Colors.black87,
+              fontFamily: 'Pacifico-Regular',  
+            ),
+          ),
+
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.blueGrey[600],
+          child: const Text('Click'),
+        )),
   ));
 }
