25d73600d360569e771b8155bf71246283376fcc
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-10-23 15:05:18 +0500

    Day 1 textbutton icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
 
 void main() {
+  
   runApp(const MaterialApp(
     home: Home(),
   ));
 }
 
+final logger = Logger();
 class Home extends StatelessWidget {
+  
+
   const Home({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +29,11 @@ class Home extends StatelessWidget {
         
         body:  Center(
           child: TextButton.icon(
-            onPressed: () {},
+            onPressed: () {logger.i('email button clicked');},
             icon: const Icon(
               Icons.mail,
             ),
-            label: const Text('Click Me'),
+            label: const Text('email me'),
             style: TextButton.styleFrom(
               foregroundColor: Colors.white,
               backgroundColor: Colors.blueGrey[600],
