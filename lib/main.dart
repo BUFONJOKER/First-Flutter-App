77cd91e366d8112ceffec9b1dcd779719f4b721d
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-10-23 12:20:59 +0500

    Day 1 statesless widget created and after saving it will automatically enabled hot reload functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(
+    const MaterialApp(
+    home:Home(),
+  )
+  );
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: const Text('My First Flutter App'),
+          title: const Text(
+            'My First Flutter App',            
+            ),
           centerTitle: true,
           backgroundColor: Colors.blueGrey[600],
         ),
         body: const Center(
           child: Text(
-            "Hello World",
+            "Hello World!",
             style: TextStyle(
-              fontSize: 20.0,
+              fontSize: 25.0,
               fontStyle: FontStyle.italic,
               letterSpacing: 2,
               color: Colors.black87,
@@ -25,6 +39,6 @@ void main() {
           onPressed: () {},
           backgroundColor: Colors.blueGrey[600],
           child: const Text('Click'),
-        )),
-  ));
+        ));
+  }
 }
